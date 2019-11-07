b7f4aec48505a90a81db05fc3f8fd2bcca57de61
Eduarda <meduardagalindo8@gmail.com>
2019-11-07 15:23:23 -0300

    Optimized and added to tpfa.py

--- heights.py
+++ heights.py
@@ -1,46 +1,38 @@
 import numpy as np
 import math
 
-class Heights:
 
-    def getHeights(M,facesID):
-        faces_nodesID = M.faces.connectivities[facesID[:]] #return the nodes global ID
-        volume_center_coords = M.volumes.center(M.volumes())
-        adjacents_volumesID = M.faces.bridge_adjacencies(facesID[:],2,3)
-        n = M.faces.normal[facesID]
+def getHeights(M,facesID):
+    faces_nodesID = M.faces.connectivities[facesID[:]] #return the nodes global ID
+    adjacents_volumesID = M.faces.bridge_adjacencies(facesID[:],2,3)
+    volume_center_coords = M.volumes.center(M.volumes())
+    adjacents_volumes_coords = volume_center_coords[adjacents_volumesID[:,:]]
+
+    face_nodes_coords = M.nodes.coords[faces_nodesID[:,0]] #one is enough
+    normal_vector = M.faces.normal[facesID]
+    normal_vector_modulus = np.linalg.norm(normal_vector,axis=1)
+    d_coefficient = -(normal_vector*face_nodes_coords).sum(axis=1) # from the plane equation (a*x+b*y+c*z+d = 0)
 
 
-        face_nodes_coords = M.nodes.coords[faces_nodesID[:,0]] #one is enough
-        d = -(n*face_nodes_coords).sum(axis=1)#(n[:,0]*face_nodes_coords[:,0]+n[:,1]*face_nodes_coords[:,1]+n[:,2]*face_nodes_coords[:,2])
-        coords_vol = volume_center_coords[adjacents_volumesID[:,:]]
-        nn = np.zeros(coords_vol.shape)
+    normal_vector_reshape = np.ones(adjacents_volumes_coords.shape)*normal_vector[:,np.newaxis,:]
+    d_coefficient_reshape = np.ones(adjacents_volumesID.shape)*d_coefficient[:,np.newaxis]
+    normal_vector_modulus_reshape = np.ones(adjacents_volumesID.shape)*normal_vector_modulus[:,np.newaxis]
 
-        if len(adjacents_volumesID[0])>1:
-            nn[:,0,:] = n
-            nn[:,1,:] = n
-            dd = np.zeros((len(facesID),2))
-            dd[:,0] = d
-            dd[:,1] = d
-            aa = np.zeros((len(facesID),2))
-            aa[:,0] = ((n*n).sum(axis=1))**(1/2)
-            aa[:,1] = ((n*n).sum(axis=1))**(1/2)
-            heights = abs((nn*coords_vol).sum(axis=2)+dd)/aa
+    heights = abs((normal_vector_reshape*adjacents_volumes_coords).sum(axis=2)+d_coefficient_reshape)/normal_vector_modulus_reshape
 
-        else:
-            nn[:,0,:] = n
-            dd = np.zeros((len(facesID),1))
-            dd[:,0] = d
-            aa = np.zeros((len(facesID),1))
-            aa[:,0] = ((n*n).sum(axis=1))**(1/2)
-            heights = abs((nn*coords_vol).sum(axis=2)+dd)/aa
+    ''' OBS:
+        normal_vector_reshape: reshaping the normal vector to the same shape as coords_vol
+        d_coefficient_reshape: reshaping the d coefficient vector to the same shape as the hights
+        normal_vector_modulus_reshape: reshaping the normal vector modulus term to the same shape as the hights
+    '''
+    return heights
 
-        return heights
+def compute_heights(self):
+    M = self.mesh
+    internal_facesID = M.faces.internal[:]
+    boundary_facesID = M.faces.boundary[:]
+    internal_heights = Heights.getHeights(M,internal_facesID)
+    boundary_heights = Heights.getHeights(M,boundary_facesID)
 
-    def compute_heights(self):
-        M = self.mesh #just in case self.mesh starts to call self.M for example
-        internal_facesID = M.faces.internal[:]
-        boundary_facesID = M.faces.boundary[:]
-        internal_heights = Heights.getHeights(M,internal_facesID)
-        boundary_heights = Heights.getHeights(M,boundary_facesID)
-        return boundary_heights, internal_heights
+    return boundary_heights, internal_heights
 #a*coords_vol[:,0]+b*coords_vol[:,1]+c*coords_vol[:,2]
--- tpfa.py
+++ tpfa.py
@@ -13,8 +13,34 @@ class tpfaScheme(object):
     def run(self):
         self.T, self.Q = self.assembly_tpfa()
 
+    def getHeights(self,facesID):
+        faces_nodesID = self.M.faces.connectivities[facesID[:]] #return the nodes global ID
+        adjacents_volumesID = self.M.faces.bridge_adjacencies(facesID[:],2,3)
+        volume_center_coords = self.M.volumes.center(M.volumes())
+        adjacents_volumes_coords = volume_center_coords[adjacents_volumesID[:,:]]
+
+        face_nodes_coords = self.M.nodes.coords[faces_nodesID[:,0]] #one is enough
+        normal_vector = self.M.faces.normal[facesID]
+        normal_vector_modulus = np.linalg.norm(normal_vector,axis=1)
+        d_coefficient = -(normal_vector*face_nodes_coords).sum(axis=1) # from the plane equation (a*x+b*y+c*z+d = 0)
+
+        ''' Reshaping for vectorized calculation
+                normal_vector_reshape: reshaping the normal vector to the same shape as coords_vol
+                d_coefficient_reshape: reshaping the d coefficient vector to the same shape as the hights
+                normal_vector_modulus_reshape: reshaping the normal vector modulus term to the same shape as the hights'''
+
+        normal_vector_reshape = np.ones(adjacents_volumes_coords.shape)*normal_vector[:,np.newaxis,:]
+        d_coefficient_reshape = np.ones(adjacents_volumesID.shape)*d_coefficient[:,np.newaxis]
+        normal_vector_modulus_reshape = np.ones(adjacents_volumesID.shape)*normal_vector_modulus[:,np.newaxis]
+
+        heights = abs((normal_vector_reshape*adjacents_volumes_coords).sum(axis=2)+d_coefficient_reshape)/normal_vector_modulus_reshape
+        return heights
+
     def compute_heights(self):
-        #compute compute_heights
+        internal_facesID = self.mesh.faces.internal[:]
+        boundary_facesID = self.mesh.faces.boundary[:]
+        internal_heights = Heights.getHeights(self,internal_facesID)
+        boundary_heights = Heights.getHeights(self,boundary_facesID)
         return boundary_heights, internal_heights
 
     def compute_eq_permeability(self):
