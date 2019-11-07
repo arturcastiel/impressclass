cf2aa77be0a2f635e7c356c7ba55d85c3757d7f7
JP <jprandrade2@gmail.com>
2019-11-07 12:34:44 -0300

    remove_tpfa_functions

--- tpfa.py
+++ tpfa.py
@@ -4,8 +4,8 @@ import pdb
 class tpfaScheme(object):
     def __init__(self, mesh):
         self.mesh = mesh
-        self.hb, self.hi = self.compute_heights()
-        self.k_eq = self.compute_eq_permeability()
+        # self.hb, self.hi = self.compute_heights()
+        # self.k_eq = self.compute_eq_permeability()
 
     def __call__(self):
         self.run()
@@ -22,19 +22,41 @@ class tpfaScheme(object):
         ''' compute equivalent permeability project on each
             face of the domain '''
 
-        M = self.mesh
-        hi = self.hi
-        hb = self.hb
-        all_faces = M.faces.all
-        internal_faces = M.faces.internal
-        b_faces = M.faces.boundary
-        u_normal = M.faces.normal[all_faces]
-        adjacencies_internal_faces = M.faces.bridge_adjacencies(internal_faces, 2, 3)
-        adjacencies_boundary_faces = M.faces.bridge_adjacencies(b_faces, 2, 3)
-        perms = M.permeability[:]
-        from tpfa_functions.eq_permeability import k_eq
+        all_faces = self.mesh.faces.all
+        internal_faces = self.mesh.faces.internal
+        b_faces = self.mesh.faces.boundary
+        u_normal = np.absolute(self.mesh.faces.normal[all_faces])
+        adjacencies_internal_faces = self.mesh.faces.bridge_adjacencies(internal_faces, 2, 3)
+        adjacencies_boundary_faces = self.mesh.faces.bridge_adjacencies(b_faces, 2, 3)
+        perms = self.mesh.permeability[:]
 
-        return k_eq(hb, hi, all_faces, internal_faces, b_faces, u_normal, adjacencies_internal_faces, adjacencies_boundary_faces, perms)
+        keq = np.zeros(len(all_faces))
+
+        k00 = self.get_perm_proj(perms[adjacencies_internal_faces[:, 0]].reshape([len(internal_faces), 3, 3]), u_normal[internal_faces])
+        k11 = self.get_perm_proj(perms[adjacencies_internal_faces[:, 1]].reshape([len(internal_faces), 3, 3]), u_normal[internal_faces])
+        keq[internal_faces] = self.hi.sum(axis=1)/(self.hi[:, 0]/k00 + self.hi[:, 1]/k11)
+
+        keq[b_faces] = self.get_perm_proj(perms[adjacencies_boundary_faces.flatten()].reshape([len(b_faces), 3, 3]), u_normal[b_faces])
+
+        return keq
+
+    def get_perm_proj(self, perms, unit_vectors):
+        '''
+        obtem a permeabilidade projetada na face
+        input:
+            perms: permeabilidade dos volumes
+            unit_vectors: vetor normal unitario das faces
+        output:
+            permeabilidade projetada
+        '''
+
+        tt = np.zeros(perms.shape)
+        tt[:, 0, :] = unit_vectors
+        tt[:, 1, :] = unit_vectors
+        tt[:, 2, :] = unit_vectors
+        tt = perms*tt
+        return tt.sum(axis=2).sum(axis=1)
+
 
     def assembly_tpfa(self):
 
